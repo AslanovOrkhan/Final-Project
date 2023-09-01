a3cff80272e7dcf3226a158276c52a1b042e8840
orxan97 <orkhanna@code.edu.az>
2023-09-01 18:45:05 +0400

    header sectionb the end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,24 @@
 // header section start
-let navbar = document.querySelector(".navbar");
+let responsivNavbar = document.querySelector(".responsive-header-container");
 document.getElementById("menu-btn").onclick = () => {
-  navbar.classList.toggle("active");
+  responsivNavbar.classList.toggle("active");
   cartItem.classList.remove("active");
   search.classList.remove("active");
 };
 let search = document.querySelector(".search-form");
 document.getElementById("search-btn").onclick = () => {
   search.classList.toggle("active");
-  navbar.classList.remove("active");
+  responsivNavbar.classList.remove("active");
   cartItem.classList.remove("active");
 };
 let cartItem = document.querySelector(".cart-items-container");
 document.getElementById("cart-btn").onclick = () => {
   cartItem.classList.toggle("active");
-  navbar.classList.remove("active");
+  responsivNavbar.classList.remove("active");
   search.classList.remove("active");
 };
 window.onscroll = () => {
-  navbar.classList.remove("active");
+  responsivNavbar.classList.remove("active");
   cartItem.classList.remove("active");
   search.classList.remove("active");
 };
