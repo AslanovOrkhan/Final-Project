6142835172cff69cb8da5bec8fa3e63ad7b1f6c6
orxan97 <orkhanna@code.edu.az>
2023-09-02 17:11:15 +0400

    home page scroll top

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,26 @@ var swiper = new Swiper(".swiper-container", {
   },
 });
 // swipper slider start
+// scroll top start
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+// scroll top end
