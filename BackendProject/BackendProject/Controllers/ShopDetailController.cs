68aae1266ccca0fe0bf360b5864c85367fd86915
orxan97 <orkhanna@code.edu.az>
2023-09-14 18:03:38 +0400

    front to back

---
+++ BackendProject/BackendProject/Controllers/ShopDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProject.Controllers
+{
+    public class ShopDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
